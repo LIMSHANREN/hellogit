c81a4ac34053c84af9cf583baf672141630db64a
Shan Ren Lim <eleneieryshanren@gmail.com>
2026-05-12 21:40:31 +0800

    Upgrade output

--- T4Q1.cpp
+++ T4Q1.cpp
@@ -15,7 +15,7 @@ public:
     Purchase(string name, int quantity, float p);
     
     void set_data(string name, int quantity, float p);
-    void calculate();
+    void calculate(float&grandPrice);
     void print();
 };
 
@@ -31,34 +31,37 @@ void Purchase::set_data(string name, int quantity, float p) {
     total = 0.0;
 }
 
-void Purchase::calculate() {
+void Purchase::calculate(float &grandPrice) {
     total = qty * price;
+    grandPrice += total;
 }
 
 void Purchase::print() {
-    cout << "----------------------------\n" << 
+    cout << "---------------------------------\n" << 
          left << setw(15) << "Item" << ": " << itemName << "\n" <<
          left << setw(15) << "Subtotal" << ": RM" << fixed << setprecision(2) << total << "\n" <<
-         "----------------------------\n" << endl;
+         "---------------------------------\n" << endl;
 }
 
 int main() {
     string name;
     int quantity;
     float price;
+    int grandQty = 0; 
+    float grandPrice = 0.0;
 
     Purchase p1("None", 0, 0.0);
 
     cout << "Welcome to the Purchase System (Enter 'q' to quit)" << endl;
     
     while(true){
-        cout << "Enter item name: ";
+        cout << left << setw(15) << "Enter item name" << ": ";
         cin >> name;
         if(name == "q" || name == "Q") break;
 
-        cout << "Enter quantity: ";
+        cout << left << setw(15) << "Enter quantity" << ": ";
         cin >> quantity;
-        cout << "Enter price: RM";
+        cout << left << setw(15) << "Enter price" << ": RM";
         cin >> price;
 
         if(price < 0 || quantity < 0){
@@ -67,9 +70,16 @@ int main() {
         }
 
         p1.set_data(name, quantity, price);
-        p1.calculate();
+        p1.calculate(grandPrice);
         p1.print();
+        grandQty += quantity;
     } 
+    cout << "==================================" << endl;
+    cout << "            GRAND TOTAL           " << endl;
+    cout << "==================================" << endl;
+    cout << left << setw(15) << "Total Items" << ": " << grandQty << endl;
+    cout << left << setw(15) << "Total Amount" << ": RM" << fixed << setprecision(2) << grandPrice << endl;
+    cout << "==================================" << endl;
 
     cout << "Exiting system. Thank you!" << endl;
     return 0; 

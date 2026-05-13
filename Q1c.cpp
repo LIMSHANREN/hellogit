772e4f8ea4ec1e8bc31103fcb595772eaf2713a2
Shan Ren Lim <eleneieryshanren@gmail.com>
2026-05-13 13:18:09 +0800

    Upgrade Q1c

--- Q1c.cpp
+++ Q1c.cpp
@@ -5,24 +5,22 @@ using namespace std;
 
 class Point {
 private:
-
-public:
     double x, y;
-    Point(double x = 0, double y = 0){
-        this->x = x;
-        this->y = y;
-    }
+public:
+    Point(const double x = 0, const double y = 0) : x(x), y(y) {}
+    double getX()const { return x; };
+    double getY()const { return y; };
 };
 
 class Line {
 private:
     Point p1, p2;
 public:
-    Line(Point p1, Point p2) : p1(p1), p2(p2) {}
+    Line(const Point &p1, const Point &p2) : p1(p1), p2(p2) {}
 
-    double length() {
-        double dx = p2.x - p1.x;
-        double dy = p2.y - p1.y;
+    double length() const {
+        double dx = p2.getX() - p1.getX();
+        double dy = p2.getY() - p1.getY();
         return sqrt(dx * dx + dy * dy);
     }
 

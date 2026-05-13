fa3690aac8d6465b923a176f9076082cff64a585
Shan Ren Lim <eleneieryshanren@gmail.com>
2026-05-13 12:39:34 +0800

    Done Q1b

---
+++ Q1b.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+using namespace std;
+
+class BankAccount {
+private:
+    double balance;
+public:
+    BankAccount()  { balance = 0.0; }
+    void Deposit(double amount){
+        balance += amount;
+    } 
+    void Withdraw(double amount){
+        if(amount > balance){
+            cout << "Insufficient funds." << endl;
+        }else{
+            balance -= amount;
+        }
+    }
+    double GetBalance() {
+        return balance;
+    }
+};
+
+void PrintBalance(BankAccount account);
+
+int main(){
+    BankAccount account;
+    account.Deposit(1000);
+    PrintBalance(account);
+    account.Withdraw(500);
+    PrintBalance(account);
+    account.Withdraw(600);
+    PrintBalance(account);
+
+    return 0;
+}
+
+void PrintBalance(BankAccount account){
+    cout << "Current balance: " << account.GetBalance() << endl;
+}
